c40ab5e4888269c0e8f87204250b9188a2682cda
GabrielCosta <ribeirocosta.rj1@gmail.com>
2023-05-03 18:09:13 +0100

    Created new activity ProductListActivity.kt and xml file to display images, also created a recyclerview to show a list of products

---
+++ app/src/main/java/com/example/ecommerceapp/ui/login/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerceapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerceapp.R
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+    }
+}
