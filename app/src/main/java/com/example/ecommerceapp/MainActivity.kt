ac8fc6d50f23873c52caa71b23891fad8c7129ec
GabrielCosta <ribeirocosta.rj1@gmail.com>
2023-05-03 19:13:36 +0100

    added fun to start new activity on click

--- app/src/main/java/com/example/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.ecommerceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecommerceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.signIn.setOnClickListener{
+            val intent = Intent(this@MainActivity, ProductListActivity::class.java)
+            intent.putExtra("login", binding.loginText.toString())
+            startActivity(intent)
+        }
+
+        binding.forgotPassword.setOnClickListener {
+            val intent = Intent(this@MainActivity, ProductListActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.signUp.setOnClickListener{
+            val intent = Intent(this@MainActivity, ProductListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
