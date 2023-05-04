cae88127dcf013dff7ebd4865bb9c3fed81e933b
ribeirocosta.rj1@gmail.com <Biel1234>
2023-05-04 18:23:54 +0100

    extracted fun getCategory from getData in order not to be called everytime, added onFailure toast just to show it was failed, call now works fine.

--- app/src/main/java/com/example/ecommerceapp/ProductListActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/ProductListActivity.kt
@@ -1,9 +1,12 @@
 package com.example.ecommerceapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ecommerceapp.Constants.BASE_URL
 import com.example.ecommerceapp.databinding.ActivityProductListBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,40 +16,53 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ProductListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductListBinding
-    var BASE_URL = "https://fakestoreapi.com"
-    lateinit var categories: List<CategoryProduct>
+    val categories = mutableListOf<CategoryProduct>()
+    private val adapterCategory by lazy {
+        CategoryAdapter(categoryList = categories, this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getCategory()
 
-        binding.recyclerView.adapter = CategoryAdapter(categoryList = categories, this)
+        binding.recyclerView.adapter = adapterCategory
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        getData()
     }
 
-    private fun getData() {
-        var retrofit = Retrofit.Builder()
+    private fun getData(): ApiInterface {
+        return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiInterface::class.java)
+    }
 
-        var retroData = retrofit.getCategory()
-        retroData.enqueue(object : Callback<List<CategoryProduct>> {
+    private fun getCategory() {
+        getData().getCategory().enqueue(object : Callback<List<String>> {
+            @SuppressLint("NotifyDataSetChanged")
             override fun onResponse(
-                call: Call<List<CategoryProduct>>,
-                response: Response<List<CategoryProduct>>
+                call: Call<List<String>>,
+                response: Response<List<String>>
             ) {
-                var data = response.body()
-                Log.d("data", data.toString())
+                val data = response.body()
+                data?.let {
+                    val category = it.mapIndexed { index, name ->
+                        CategoryProduct(
+                            name = name,
+                            image = getImages()[index]
+                        )
+                    }
+                    categories.addAll(category)
+                    adapterCategory.notifyDataSetChanged()
+                }
+                Log.d("responseBody", data.toString())
             }
 
-            override fun onFailure(call: Call<List<CategoryProduct>>, t: Throwable) {
-                TODO("Not yet implemented")
+            override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                Toast.makeText(this@ProductListActivity, "Error", Toast.LENGTH_SHORT).show()
             }
-
         })
     }
 }
