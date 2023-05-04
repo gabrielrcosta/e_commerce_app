4cbfd847eadb84c24e9ec2f339a219cb1e50de38
GabrielCosta <ribeirocosta.rj1@gmail.com>
2023-05-04 23:40:58 +0100

    Created a new activity, fun goToDetailsProducts to really open a new activity retrofit call added with getDetails function button functionality

--- app/src/main/java/com/example/ecommerceapp/ProductListActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/ProductListActivity.kt
@@ -1,24 +1,21 @@
 package com.example.ecommerceapp
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.ecommerceapp.Constants.BASE_URL
+import com.example.ecommerceapp.Retrofit.getData
 import com.example.ecommerceapp.databinding.ActivityProductListBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class ProductListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductListBinding
-    val categories = mutableListOf<CategoryProduct>()
+    private val categoriesList = mutableListOf<CategoryProduct>()
     private val adapterCategory by lazy {
-        CategoryAdapter(categoryList = categories, this)
+        CategoryAdapter(categoriesList, this, goToDetails())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,17 +28,8 @@ class ProductListActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
-    private fun getData(): ApiInterface {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiInterface::class.java)
-    }
-
     private fun getCategory() {
         getData().getCategory().enqueue(object : Callback<List<String>> {
-            @SuppressLint("NotifyDataSetChanged")
             override fun onResponse(
                 call: Call<List<String>>,
                 response: Response<List<String>>
@@ -54,15 +42,24 @@ class ProductListActivity : AppCompatActivity() {
                             image = getImages()[index]
                         )
                     }
-                    categories.addAll(category)
+                    categoriesList.addAll(category)
                     adapterCategory.notifyDataSetChanged()
                 }
-                Log.d("responseBody", data.toString())
             }
 
             override fun onFailure(call: Call<List<String>>, t: Throwable) {
                 Toast.makeText(this@ProductListActivity, "Error", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+    private fun goToDetails(): Click {
+        return object : Click {
+            override fun onClick(name: String) {
+                val intent = Intent(this@ProductListActivity, ProductDetailsActivity::class.java)
+                intent.putExtra("Extra", name)
+                startActivity(intent)
+            }
+
+        }
     }
 }
