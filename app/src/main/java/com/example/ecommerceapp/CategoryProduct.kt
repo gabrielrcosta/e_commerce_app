c272a37d232a0ecc2b21282233c3742e953a3d6e
ribeirocosta.rj1@gmail.com <Biel1234>
2023-05-04 18:08:16 +0100

    Created a function to get images online and apply on the apps second screen. also changed the data received on the call not getting anymore products but name and image.

--- app/src/main/java/com/example/ecommerceapp/CategoryProduct.kt
+++ app/src/main/java/com/example/ecommerceapp/CategoryProduct.kt
@@ -1,8 +1,20 @@
 package com.example.ecommerceapp
 
+import com.example.ecommerceapp.Constants.ELECTRONIC_IMAGE
+import com.example.ecommerceapp.Constants.JEWELLERY_IMAGE
+import com.example.ecommerceapp.Constants.MENS_WEAR_IMAGE
+import com.example.ecommerceapp.Constants.WOMENS_WEAR_IMAGE
+
 data class CategoryProduct(
-    val electronics: String,
-    val jewelery: String,
-    val mensClothing: String,
-    val womensClothing: String
+    val name: String,
+    val image: String
 )
+
+fun getImages(): MutableList<String> {
+    val newCategoryList = mutableListOf<String>()
+    newCategoryList.add(ELECTRONIC_IMAGE)
+    newCategoryList.add(JEWELLERY_IMAGE)
+    newCategoryList.add(MENS_WEAR_IMAGE)
+    newCategoryList.add(WOMENS_WEAR_IMAGE)
+    return newCategoryList
+}
