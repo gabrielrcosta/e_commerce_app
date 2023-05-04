d82671a87fef976e0eb3f359e378680259d4fea2
GabrielCosta <ribeirocosta.rj1@gmail.com>
2023-05-04 23:28:31 +0100

    added new fun getProduct to get response from api

--- app/src/main/java/com/example/ecommerceapp/ApiInterface.kt
+++ app/src/main/java/com/example/ecommerceapp/ApiInterface.kt
@@ -2,8 +2,15 @@ package com.example.ecommerceapp
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiInterface {
     @GET("/products/categories")
     fun getCategory(): Call<List<String>>
+
+    @GET("products/category/{category}")
+    fun getDetails(@Path("category") category:String): Call<List<CategoryDetails>>
+
+    @GET("/products/{numberProduct}")
+    fun getProduct(@Path("numberProduct") numberProduct:Int): Call<CategoryDetails>
 }
