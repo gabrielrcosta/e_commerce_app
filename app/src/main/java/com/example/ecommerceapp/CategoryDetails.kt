e4fd97034f437f9bc94f3149fb3bd53e1604d87f
GabrielCosta <ribeirocosta.rj1@gmail.com>
2023-05-04 23:31:54 +0100

    Created a new data class model CategoryDetails.kt

---
+++ app/src/main/java/com/example/ecommerceapp/CategoryDetails.kt
@@ -0,0 +1,10 @@
+package com.example.ecommerceapp
+
+data class CategoryDetails(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val image: String,
+    val category: String
+)
