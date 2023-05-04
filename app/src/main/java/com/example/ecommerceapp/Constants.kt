30515a5ccc3d1a698d6aeb097d1523f30dd20101
ribeirocosta.rj1@gmail.com <Biel1234>
2023-05-04 17:56:08 +0100

    Created a new object class to hold all the Constants.kt of the application.

---
+++ app/src/main/java/com/example/ecommerceapp/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.ecommerceapp
+
+object Constants {
+    val BASE_URL = "https://fakestoreapi.com"
+    val ELECTRONIC_IMAGE = "https://cgu-odisha.ac.in/wp-content/uploads/2021/04/electronic-engineering-1.jpg"
+    val JEWELLERY_IMAGE = "https://media.istockphoto.com/id/494833184/photo/shiny-gold-and-silver-jewelery.jpg?s=612x612&w=0&k=20&c=IyxoyEJuKNpTkiuoFMsw4wJT9fI-r0MkLJ0JxOrcg44="
+    val WOMENS_WEAR_IMAGE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ErpZmr9IFKyLfs3Bzv8BV-_s7hWyJ-Glu_K58BjUuKaaKBiIIM1Mqawuo5REaRHgwR8&usqp=CAU"
+    val MENS_WEAR_IMAGE = "https://cdn.shopify.com/s/files/1/0075/3929/4305/collections/GIFTS_800x800.jpg?v=1668573560"
+}
